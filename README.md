dc9ff470e60b05246e22305dc10e1b1917268e04
Shubham Chitranshi <shubam.chitranshi@truata.com>
2021-07-04 19:18:28 +0100

    Authentication Added Authentication Keyclock verify audience verify signature

--- README.md
+++ README.md
@@ -9,4 +9,4 @@ cd dev folder - `docker compose -f mongo-db-docker-compose.yaml up`
 `docker run -e KEYCLOAK_USER=admin -e KEYCLOAK_PASSWORD=admin -p 8080:8080 jboss/keycloak`
 import the `realm-export.json` file in the keyclock and create a user with todos_roles
 admin
-
+Reading - https://auth0.com/blog/spring-boot-authorization-tutorial-secure-an-api-java/
--- src/main/java/com/learn/springalltodos/configurations/securities/SecurityConfig.java
+++ src/main/java/com/learn/springalltodos/configurations/securities/SecurityConfig.java
@@ -1,12 +1,24 @@
 package com.learn.springalltodos.configurations.securities;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.core.DelegatingOAuth2TokenValidator;
+import org.springframework.security.oauth2.core.OAuth2TokenValidator;
+import org.springframework.security.oauth2.jwt.*;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+
+    @Value("${idm.audience}")
+    private String audience;
+
+    @Value("${spring.security.oauth2.resourceserver.jwt.issuer-uri}")
+    private String issuer;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
@@ -15,6 +27,17 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .authenticated()
                 .and()
                 .oauth2ResourceServer()
-                .jwt();
+                .jwt()
+                .decoder(jwtDecoder());
+    }
+
+    private JwtDecoder jwtDecoder() {
+        OAuth2TokenValidator<Jwt> withAud = new AudienceValidator(audience);
+        OAuth2TokenValidator<Jwt> withIssuer = JwtValidators.createDefaultWithIssuer(issuer);
+        OAuth2TokenValidator<Jwt> validator = new DelegatingOAuth2TokenValidator<>(withAud, withIssuer);
+
+        NimbusJwtDecoder jwtDecoder = (NimbusJwtDecoder) JwtDecoders.fromOidcIssuerLocation(issuer);
+        jwtDecoder.setJwtValidator(validator);
+        return jwtDecoder;
     }
 }
